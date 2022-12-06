8386e52418a2f1fefe13e2d49ba8ce1b2af37043
mahonk89 <mahonk5692@gmail.com>
2022-12-06 12:49:27 -0800

    See comments of code

--- Testing1.R
+++ Testing1.R
@@ -2,3 +2,6 @@ print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
 
+"Tools > Shell > run command: git push > log in > Sucessful push"
+
+"adding additional comments to push via Rstudio to see if fix was successful"
