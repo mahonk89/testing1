12911c20e6d0c9e72dc0e70c67c5542f593f763b
mahonk89 <mahonk5692@gmail.com>
2022-12-06 12:52:05 -0800

    next test message

--- Testing1.R
+++ Testing1.R
@@ -5,3 +5,5 @@ print("And now it lives on GitHub")
 "Tools > Shell > run command: git push > log in > Sucessful push"
 
 "adding additional comments to push via Rstudio to see if fix was successful"
+print("Apparently the commit was successful, however github is not showing the 
+      updated comments. Running stage>commit>push test again")
